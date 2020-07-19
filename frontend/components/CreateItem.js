43088f287b9a63bf2eecb3caa260b815bbe7b741
smarttin <smarttin77@gmail.com>
2020-07-19 09:27:11 +0100

    Uploading Images

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -40,26 +40,58 @@ class CreateItem extends Component {
     this.setState({ [name]: val });
   };
 
+  uploadFile = async (event) => {
+    const files = event.target.files;
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('upload_preset', 'sickfits');
+    const res = await fetch(
+      'https://api.cloudinary.com/v1_1/dfrz86zh1/image/upload/',
+      {
+        method: 'POST',
+        body: data,
+      }
+    );
+    const file = await res.json();
+    console.log(file);
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url,
+    });
+  };
+
   render() {
     return (
       <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
         {(createItem, { loading, error }) => (
           <Form
-            onSubmit={async event => {
-            // Stop the form from submitting
-            event.preventDefault();
-            // call the mutation
-            const res = await createItem();
-            // change them to the single item page
-            console.log(res);
-            Router.push({
-              pathname: '/item',
-              query: { id: res.data.createItem.id },
-            });
-          }}
+            onSubmit={async (event) => {
+              // Stop the form from submitting
+              event.preventDefault();
+              // call the mutation
+              const res = await createItem();
+              // change them to the single item page
+              console.log(res);
+              Router.push({
+                pathname: '/item',
+                query: { id: res.data.createItem.id },
+              });
+            }}
           >
-            <Error error={error}/>
+            <Error error={error} />
             <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="title">
+                Image
+                <input
+                  type="file"
+                  id="file"
+                  name="file"
+                  required
+                  onChange={this.uploadFile}
+                />
+                {this.state.image && <img width='200' src={this.state.image} alt='Upload Preview' />}
+              </label>
+
               <label htmlFor="title">
                 Title
                 <input
